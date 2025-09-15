03546c2b9f7cf810b78fdafaa6a8557287f7cc24
Aniqa <aniqashafiq510@gmail.com>
2025-09-15 21:06:42 +0500

    changes saved

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Button from '@/components/button'
+import React from 'react'
+
+
+export default function page() {
+  return (
+    <div> this is  home page
+
+      <Button/>
+    </div>
+    
+  )
+}
+
+
+
+// rfc shortcut for making this function
---
+++ components/button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <div className='cursor-pointer border-1 border-white bg-blue-400 m-4 w-54 hover:bg-blue-700'>Click me!</div>
+  )
+}
