1bafdfddeb4dd0b9f765fb851921dbf69eba9c82
Aniqa <aniqashafiq510@gmail.com>
2025-09-16 20:44:04 +0500

    event on button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
 import Button from '@/components/button'
 import React from 'react'
 
+const Fruits = [
+  {name: "Apple", id : 1 },
+  {name: "Mango", id : 2 },
+  {name: "Grapes", id : 3 }
+]
 
+// const user = {
+//   name: 'Aniqa',
+//   age: 21,
+//   height : '5-3'
+// }
 export default function page() {
+  // const isLoggedin = true;
   return (
-    <div> this is  home page
-
+    <div> 
+{/* key is given to track that which element is changed or called, it should be unique */}
+      {/* {Fruits.map(fruit => (
+        <div key={fruit.id}>
+          <p>
+            {fruit.name}
+          </p>
+        </div>
+      ))} */}
       <Button/>
+      {/* {<p>{user.name}</p>
+      } */}
+
+     {/* {isLoggedin?  <Button/>: 'Please Login to continue!'} */}
     </div>
     
   )
--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,18 @@
+"use client"
+
+// use client sb sy uper likhna h wrna error dy ga
+
 import React from 'react'
 
+
 export default function Button() {
+  const clickHandler = ()=> {
+    alert("Button has clicked!")
+  }
   return (
-    <div className='cursor-pointer border-1 border-white bg-blue-400 m-4 w-54 hover:bg-blue-700'>Click me!</div>
+    <div onClick={clickHandler} className=' transition-colors duration-300 cursor-pointer text-black border-1 border-white bg-gray-300 m-4 w-54 hover:text-white hover:bg-gray-500'>Click me!</div>
   )
 }
+
+
+// RENDERING...conversion of code into UI
