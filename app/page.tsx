f77b2b3fe9b2cc953a499c766cbde815c206ac75
Aniqa <aniqashafiq510@gmail.com>
2025-09-27 14:58:51 +0500

    useEffect

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function page() {
   return (
     <div> 
 
-
+      
       <Input/>
 
       {/* <ProfileCard
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,12 +1,16 @@
-
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Input = () => {
+  
     const [name,setname] = useState("")
     const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setname(event.target.value)
     }
+    useEffect(()=> {
+      console.log('UseEffect is working👍')
+      
+  })
   return (
     <div className='mt-10 flex flex-col justify-center items-center space-y-4'>
         <input type="text" placeholder='Enter your name....' value={name} className='p-2 outline-none ring-2
